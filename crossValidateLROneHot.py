5a47343b2954260b6b2ffa16b373229e1013ba64
warshmellow <ddwchensf@gmail.com>
2013-06-20 00:59:23 -0700

    Add Logistic Regression and LinearSVC feature selection to Logistic Regression

--- crossValidateLROneHot.py
+++ crossValidateLROneHot.py
@@ -8,32 +8,54 @@ def main():
     dataset = pd.read_csv('Data/train.csv')
     target = dataset.ACTION.values
     train = dataset.drop('ACTION', axis=1).drop('ROLE_ROLLUP_1',axis=1).drop('ROLE_FAMILY', axis=1).drop('ROLE_CODE', axis=1).values
-
-    # OneHotEncoding
-    enc = preprocessing.OneHotEncoder()
-    enc.fit(train)
-    train = enc.transform(train)
-
-    # Use Linear model classifier (Logistic Regression)
-    cfr = LogisticRegression(C=3.0)
+    #train = dataset.drop('ACTION', axis=1).drop('ROLE_CODE',axis=1).values
 
     # Simple KFold cross validation. 10 folds.
-    #cv = cross_validation.KFold(len(train), n_folds=10, indices=False)
     cv = cross_validation.KFold(train.shape[0], n_folds=10, indices=True)
 
     # iterate through the training and test cross validation segments and
     # run the classifier on each one, aggregating the results into a list
     results = []
     for traincv, testcv in cv:
-        predictions = cfr.fit(train[traincv], target[traincv]).predict_proba(train[testcv])[:,1]
-        #predictions = cfr.fit(train[traincv], target[traincv]).predict(train[testcv])
-        true_labels = target[testcv]
+
+        # Test/train data
+        train_subset = train[traincv]
+        test_subset = train[testcv]
+        target_train_subset = target[traincv]
+        target_test_subset = target[testcv]
+
+        # One Hot Encode train and test data
+        enc = preprocessing.OneHotEncoder()
+        enc.fit(train)
+        train_subset = enc.transform(train_subset)
+        test_subset = enc.transform(test_subset)
+
+        # Feature Selection
+        # Logistic Regression for Feature Selection
+        lrc = LogisticRegression(C=3.0)
+        lrc.fit(train_subset, target_train_subset)
+        train_subset = lrc.transform(train_subset)
+        test_subset = lrc.transform(test_subset)
+
+        # Feature Selection
+        # Linear Support Vector Classification for Feature Selection
+        #lsvc = svm.LinearSVC()
+        #lsvc.fit(train_subset, target_train_subset)
+        #train_subset = lsvc.transform(train_subset)
+        #test_subset = lsvc.transform(test_subset)
+
+        # Model Selection (will include Hyperparameter Optimization)
+        cfr = LogisticRegression(C=3.0)
+
+        # Predictions and Scoring
+        predictions = cfr.fit(train_subset, target_train_subset).predict_proba(test_subset)[:,1]
+        true_labels = target_test_subset
         fpr, tpr, thresholds = metrics.roc_curve(true_labels, predictions, pos_label = 1)
         auc = metrics.auc(fpr, tpr)
         results.append(auc)
 
-    for r in results:
-        print "Result: " + str(r)
+        print "Result: " + str(auc)
+
     print "Average Result: " + str( np.array(results).mean() )
 
 if __name__=="__main__":
